add91ff3cec247e6d4a08bae8bf6d3b71a1a0600
imoreno <imoreno@imorenos-MBP.fios-router.home>
2016-03-20 13:42:03 -0400

    added common assets

---
+++ src/actions.js
@@ -0,0 +1,20 @@
+import shop from '../../common/api/shop';
+import reactor from './reactor.js';
+import { RECEIVE_PRODUCTS,
+         ADD_TO_CART,
+         CHECKOUT_START,
+         CHECKOUT_SUCCESS,
+         CHECKOUT_FAILED
+       } from './actionTypes.js';
+
+export default {
+  fetchProducts() {
+    shop.getProducts(products => {
+      reactor.dispatch(RECEIVE_PRODUCTS, { products })
+    });
+  },
+
+  addToCart(product) {
+    reactor.dispatch(ADD_TO_CART, { product })
+  }
+}
